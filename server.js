9340b5eeb3050f90e46f0ec1850b1890e635e419
Rizwan <rizwan.ahmed19@outlook.com>
2018-01-28 01:44:18 +0500

    add /todos and /todos/:id endpoint

--- server.js
+++ server.js
@@ -3,8 +3,37 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let todos = [
+  {
+    id: 1,
+    description: 'Water the plants',
+    completed: false,
+  },
+  {
+    id: 2,
+    description: 'Go to market',
+    completed: false,
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Todo API root');
+});
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todoId = parseInt(req.params.id);
+
+  const todo = todos.filter(todo => todo.id === todoId);
+
+  if (todo.length > 0) {
+    res.json(todo);
+  } else {
+    res.status(404).send('Todo does not exist!');
+  }
 });
 
 app.listen(PORT, () => {
