de63c024c82bd613e4cfa1b7c39577bb26f14c7b
Rizwan <rizwan.ahmed19@outlook.com>
2018-01-28 21:33:57 +0500

    Add PUT endpoint

--- server.js
+++ server.js
@@ -62,6 +62,37 @@ app.delete('/todos/:id', (req, res) => {
   }
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', (req, res) => {
+  const body = _.pick(req.body, 'description', 'completed');
+  const todoId = parseInt(req.params.id);
+  const matchedTodo = _.findWhere(todos, { id: todoId });
+  const validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+
+  if (
+    body.hasOwnProperty('description') &&
+    _.isString(body.description) &&
+    body.description.trim().length > 0
+  ) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
+});
+
 app.listen(PORT, () => {
   console.log('App is up on port', PORT);
 });
