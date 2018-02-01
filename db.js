f14b6d12272d172c081b285660e8032f8a1fdde6
Rizwan <rizwan.ahmed19@outlook.com>
2018-02-01 15:11:33 +0500

    add postgresql for production env

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(undefined, undefined, undefined, {
-  operatorsAliases: Sequelize.Op,
-  dialect: 'sqlite',
-  storage: __dirname + '/data/dev-todo-api.sqlite',
-});
+let sequelize;
+let env = process.env.NODE_ENV || 'development';
+
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgresql',
+  });
+} else {
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+    operatorsAliases: Sequelize.Op,
+    dialect: 'sqlite',
+    storage: __dirname + '/data/dev-todo-api.sqlite',
+  });
+}
 
 let db = {};
 db.todo = sequelize.import(__dirname + '/models/todo.js');
