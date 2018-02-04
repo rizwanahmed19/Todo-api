6ab91632e53ec1ab9aa99af41978666300c320db
Rizwan <rizwan.ahmed19@outlook.com>
2018-02-04 13:30:09 +0500

    Add hooks for converting email to lowercase

--- models/user.js
+++ models/user.js
@@ -1,21 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('user', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        notEmpty: true,
-        isEmail: true,
+  return sequelize.define(
+    'user',
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [7, 100],
+        },
       },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [7, 100],
+    {
+      hooks: {
+        beforeValidate: (user, options) => {
+          if (typeof user.email === 'string') {
+            user.email = user.email.toLowerCase();
+          }
+        },
       },
     },
-  });
+  );
 };
